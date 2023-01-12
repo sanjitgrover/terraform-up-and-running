862c03f5486bb12fea9640330a6e1ef02b9ed853
EC2 Default User <ec2-user@ip-172-31-15-161.ec2.internal>
2023-01-12 11:29:46 +0000

    main changes

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_security_group" "instance"{
 resource "aws_instance" "example"{
     ami="ami-0b5eea76982371e91"
     instance_type="t2.micro"
+    vpc_security_group_ids=[aws_security_group.instance.id]
     user_data= <<-EOF
                #!/bin/bash
                echo "Hello, World" > index.html
